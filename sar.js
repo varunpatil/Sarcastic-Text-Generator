db4437ebced5acb6c8a2f3bd72fb30ba2ccbda6d
Varun Patil <31964349+varun271099@users.noreply.github.com>
2019-05-15 23:07:55 +0530

    Added Dankness

--- sar.js
+++ sar.js
@@ -2,8 +2,12 @@ var submit = function()
 {
 	s=document.getElementById("input").value;
 	s=s.toUpperCase();
-	out="";
+	out1="";
+	out2="";
 	var a=0;
+
+	var smallchar=["aãåāàáâäąă","bƄ","cçĉ¢ċč","dďđḍ","eēêĕëėęèé","fƒ","gġĝğ","hĥħ","i!ìïĭįīîí","jĵ","ķk","l","m","nñṅń","oõōøòöôó","p","qǫ","rṛŕŗř","s$śṣšş","tṭͳţŧ","uūüũùûúůű","v","wŵω","x×","y¥ýÿŷ","zƶžźžż"];
+	var bigchar=["AÃÅĀÀÁÂÄĂĄ","BẞƁɃ","CĆĈČÇĊÇ","DĎḌÐ","EƎĒÊĔËÈĘĖÉ£Ʃ","ƑF","GĠĜĢĞ","HĤ","IÌÏĪĬÎÍ","JĴ","KĶ","LŁĿĽ","M","NÑŊŃƝ","OÕŌØÒÖÔΘÓ0","PƤ","QǬǪ","RṚŔŖŘ","S$ŚṢŠŞ","TṬŤŦͲ","UŪŨÜŮŰÙÛÚ","V","WŴƜ","X","YÝŶŸ","ZƵŽŻŽŹ"];
 
 	for(var i=0;i<s.length;i++)
 	{
@@ -11,20 +15,56 @@ var submit = function()
 		{
 			if(a%2==0)
 			{
-				out=out+s[i].toLowerCase();
+				out1=out1+s[i].toLowerCase();
 			}
 			else
 			{
-				out=out+s[i];
+				out1=out1+s[i];
 			}
 			a++;
 		}
 		else
 		{
-			out=out+s[i];
+			out1=out1+s[i];
+			if(s[i]==" ")
+			{
+				a=0;
+			}
+		}
+	}
+
+	a=0;
+	for(var i=0;i<s.length;i++)
+	{
+		if(s[i]!=' ')
+		{
+			if(s[i]>='A' && s[i]<='Z')
+			{
+				var no = s.charCodeAt(i)-65;
+				if(a%2==0)
+				{
+					var num = Math.floor(Math.random() * smallchar[no].length);
+					out2=out2+smallchar[no][num];
+				}
+				else
+				{
+					var num = Math.floor(Math.random() * bigchar[no].length);
+					out2=out2+bigchar[no][num];
+				}
+				a++;
+			}
+			else
+			{
+				out2=out2+s[i];
+			}
+		}
+		else
+		{
+			out2=out2+s[i];
 			a=0;
 		}
 	}
 
-	document.getElementById("output").innerText=out;
+	document.getElementById("output1").innerText=out1;
+	document.getElementById("output2").innerText=out2;
 }
